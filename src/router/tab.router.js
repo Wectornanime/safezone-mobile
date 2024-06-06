a2c1b33b141d87bc3d7cc87173c1ae65ddf95413
Wectornanime <81574689+Wectornanime@users.noreply.github.com>
2024-06-05 22:58:55 -0300

    created settings screen

--- src/router/tab.router.js
+++ src/router/tab.router.js
@@ -1,19 +1,28 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from '@expo/vector-icons/Ionicons'
 
-import Home from "../screens/Home"; 
+import Home from "../screens/Home";
+import Settings from "../screens/Settings";
 
 const Tab = createBottomTabNavigator();
 
 export default function TabRouter() {
     return (
-        <Tab.Navigator screenOptions={{ headerShown: false, tabBarLabel: false }}>
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
             <Tab.Screen
                 name="main"
                 component={Home}
                 options={{
                     tabBarIcon: ({ color, size }) => <Ionicons name="home" color={color} size={size} />,
                     tabBarLabel: 'Home'
+                }}
+            />
+            <Tab.Screen
+                name="settings"
+                component={Settings}
+                options={{
+                    tabBarIcon: ({ color, size }) => <Ionicons name="settings" color={color} size={size} />,
+                    tabBarLabel: 'Settings'
                 }}
             />
         </Tab.Navigator>
