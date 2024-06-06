a28168b2e445f446147b3b5584f54b5ef16e4c7c
Wectornanime <81574689+Wectornanime@users.noreply.github.com>
2024-06-05 23:51:07 -0300

    added header into settings screen

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,10 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function Settings() {
     return (
-        <View style={styles.container}>
-            <Text>Hey, Settings is working!</Text>
-        </View>
+        <ScrollView style={styles.container}>
+            <View style={styles.header}>
+                <Ionicons name='settings' size={size = 80} color={color = '#8BD784'} />
+                <Text style={styles.title}>Configurações</Text>
+            </View>
+        </ScrollView>
     );
 }
 
@@ -12,7 +16,14 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
+    },
+    header: {
+        paddingTop: '15px',
         alignItems: 'center',
-        justifyContent: 'center',
+        gap: 15,
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: '900',
     },
 });
