66b96f0ee4a5f6e24d59349fa3a6e3a6016ca504
Wectornanime <81574689+Wectornanime@users.noreply.github.com>
2024-06-05 23:52:37 -0300

    added container of user datails into settings screen

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -8,6 +8,13 @@ export default function Settings() {
                 <Ionicons name='settings' size={size = 80} color={color = '#8BD784'} />
                 <Text style={styles.title}>Configurações</Text>
             </View>
+            <View style={styles.userDetails}>
+                <Image
+                    source={{ uri: 'https://freesvg.org/img/abstract-user-flat-4.png' }}
+                    style={{ width: 50, height: 50 }} 
+                />
+                <Text style={styles.text}>Usuario</Text>
+            </View>
         </ScrollView>
     );
 }
@@ -16,6 +23,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
+        padding: 10,
+        gap: 10,
     },
     header: {
         paddingTop: '15px',
@@ -25,5 +34,13 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 30,
         fontWeight: '900',
+    },
+    userDetails: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 5,
+    },
+    text: {
+        fontSize: 20,
     },
 });
