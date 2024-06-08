7b0d0d64672bea60d0cca4feaa679acce452831c
Wectornanime <81574689+Wectornanime@users.noreply.github.com>
2024-06-08 18:24:10 -0300

    finished alert screen

--- src/screens/Alert.js
+++ src/screens/Alert.js
@@ -1,9 +1,99 @@
-import { Text, View } from "react-native";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-export default function Alert() {
+export default function Alert({ route }) {
     return (
-        <View>
-            <Text>Alert is work</Text>
-        </View>
+        <ScrollView style={styles.mainContainer}>
+            <View style={styles.container}>
+                <Image
+                    style={styles.areaImage}
+                />
+                <View style={{ flexDirection: 'row', alignItems: 'center', gap: 5 }}>
+                    <Text style={styles.areaTitle}>Recepção</Text>
+                    <Ionicons name='alert-circle' color={color = '#FFE976'} size={size = 30} />
+                </View>
+                <Text style={styles.areaSubTitle}>Grupo IV: Ergonomia</Text>
+                <Text style={styles.areaTitle2}>Recepetividade</Text>
+                <Text style={styles.areaSubTitle2}>Avalie este risco:</Text>
+                <View style={styles.riskyContainer}>
+                    <MaterialCommunityIcons name="emoticon-happy" size={size = 33} color={colo = '#B0E0AC'} />
+                    <MaterialCommunityIcons name="emoticon-neutral" size={size = 33} color={colo = '#FFE976'} />
+                    <MaterialCommunityIcons name="emoticon-sad" size={size = 33} color={colo = '#FB6E6E'} />
+                </View>
+                <View style={styles.actionContainer}>
+                    <TouchableOpacity style={{ ...styles.actionButton, backgroundColor: '#FFF2B1' }}>
+                        <Text style={styles.text}>Reporte</Text>
+                        <Ionicons name="megaphone" size={size = 20} />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{ ...styles.actionButton, backgroundColor: '#FFAEAE' }}>
+                        <Text style={styles.text}>Emergência</Text>
+                        <Ionicons name='alert-circle' size={size = 20} />
+                    </TouchableOpacity>
+                </View>
+                <Image
+                    style={styles.floorplaner}
+                />
+            </View>
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        backgroundColor: '#fff',
+    },
+    container: {
+        flex: 1,
+        padding: 18,
+        alignItems: 'center',
+        gap: 10,
+    },
+    areaImage: {
+        height: 150,
+        width: '100%',
+        backgroundColor: 'red',
+    },
+    areaTitle: {
+        fontSize: 30,
+        fontWeight: '900',
+    },
+    areaSubTitle: {
+        fontSize: 30,
+        fontWeight: '200',
+        fontStyle: 'italic',
+    },
+    areaTitle2: {
+        fontSize: 30,
+        fontWeight: '600',
+    },
+    areaSubTitle2: {
+        fontSize: 20,
+        fontWeight: '200',
+        fontStyle: 'italic',
+    },
+    riskyContainer: {
+        flexDirection: 'row',
+        gap: 15,
+    },
+    actionContainer: {
+        flexDirection: 'row',
+        gap: 5,
+    },
+    actionButton: {
+        padding: 5,
+        borderRadius: 10,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 3,
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: '500',
+    },
+    floorplaner: {
+        backgroundColor: 'red',
+        width: 300,
+        height: 300,
+    },
+})
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,46 +1,51 @@
 import { Ionicons } from '@expo/vector-icons';
-import { Button, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function Home() {
+export default function Home({ navigation }) {
     return (
-            <ScrollView style={styles.container}>
-                <View>
-                    <Text style={{ fontSize: 24, fontWeight: '600' }}>Olá, usuário</Text>
-                    <Text style={{ fontSize: 20 }}>Se sentindo seguro hoje? <Ionicons name='happy' size={size = 20} color={color = '#97C993'} /></Text>
+        <ScrollView style={styles.container}>
+            <View>
+                <Text style={{ fontSize: 24, fontWeight: '600' }}>Olá, usuário</Text>
+                <Text style={{ fontSize: 20 }}>Se sentindo seguro hoje? <Ionicons name='happy' size={size = 20} color={color = '#97C993'} /></Text>
+            </View>
+            <View style={{ marginVertical: 15, alignItems: 'flex-end' }}>
+                <Text style={{ fontSize: 26, fontStyle: 'italic' }}>Opp Solutions</Text>
+            </View>
+            <View style={styles.areaIten}>
+                <Text style={styles.areaTitle}>Copa</Text>
+                <Image
+                    style={styles.areaImage}
+                />
+                <View style={styles.areaButton}>
+                    <TouchableOpacity
+                        style={styles.actionButton}
+                        onPress={() => navigation.navigate('alert')}
+                    >
+                        <Ionicons name='alert-circle' color={color = '#94C190'} size={size = 30} />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.actionButton}>
+                        <Ionicons name='help-circle' color={color = '#94C190'} size={size = 30} />
+                    </TouchableOpacity>
                 </View>
-                <View style={{ marginVertical: 15, alignItems: 'flex-end' }}>
-                    <Text style={{ fontSize: 26, fontStyle: 'italic' }}>Opp Solutions</Text>
+            </View>
+            <View style={styles.areaIten}>
+                <Text style={styles.areaTitle}>Recepção</Text>
+                <Image
+                    style={styles.areaImage}
+                />
+                <View style={styles.areaButton}>
+                    <TouchableOpacity
+                        style={styles.actionButton}
+                        onPress={() => navigation.navigate('alert')}
+                    >
+                        <Ionicons name='alert-circle' color={color = '#94C190'} size={size = 30} />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.actionButton}>
+                        <Ionicons name='help-circle' color={color = '#94C190'} size={size = 30} />
+                    </TouchableOpacity>
                 </View>
-                <View style={styles.areaIten}>
-                    <Text style={styles.areaTitle}>Copa</Text>
-                    <Image
-                        style={styles.areaImage}
-                    />
-                    <View style={styles.areaButton}>
-                        <TouchableOpacity style={styles.actionButton}>
-                            <Ionicons name='alert-circle' color={color = '#94C190'} size={size = 30} />
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.actionButton}>
-                            <Ionicons name='help-circle' color={color = '#94C190'} size={size = 30} />
-                        </TouchableOpacity>
-                    </View>
-                </View>
-                <View style={styles.areaIten}>
-                    <Text style={styles.areaTitle}>Recepção</Text>
-                    <Image
-                        style={styles.areaImage}
-                    />
-                    <View style={styles.areaButton}>
-                        <TouchableOpacity style={styles.actionButton}>
-                            <Ionicons name='alert-circle' color={color = '#94C190'} size={size = 30} />
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.actionButton}>
-                            <Ionicons name='help-circle' color={color = '#94C190'} size={size = 30} />
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </ScrollView>
+            </View>
+        </ScrollView>
     );
 }
 
