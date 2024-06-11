a6fdfa511ed1c01bdbd3ac19567d3e23717246c2
Wectornanime <81574689+Wectornanime@users.noreply.github.com>
2024-06-11 11:05:00 -0300

    finished reports screen

--- src/screens/Reports.js
+++ src/screens/Reports.js
@@ -1,14 +1,41 @@
 import { Ionicons } from '@expo/vector-icons';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function Reports({ navigation }) {
+    const [reports, setReports] = useState(null);
+
+    useEffect(() => {
+        axios.get('http://172.26.46.161:3000/reports/report')
+            .then(response => {
+                console.log(response.data);
+                setReports(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, []);
+
     return (
         <ScrollView style={styles.container}>
-            <View>
-                <Text style={{ fontSize: 24, fontWeight: '600' }}>Olá, usuário</Text>
-                <Text style={{ fontSize: 20 }}>Se sentindo seguro hoje? <Ionicons name='happy' size={size = 20} color={color = '#97C993'} /></Text>
-            </View>
-            
+            {reports && (
+                reports.map((report, index) => (
+                    <TouchableOpacity style={styles.containerItens} key={index}>
+                        <View style={styles.texts}>
+                            <View style={styles.text}>
+                                <Text style={{ fontWeight: '600', fontSize: 16 }}>Nome: </Text>
+                                <Text style={{ fontSize: 16 }}>{report.name}</Text>
+                            </View>
+                            <View style={styles.text}>
+                                <Text style={{ fontWeight: '600', fontSize: 16 }}>Status: </Text>
+                                <Text style={{ fontSize: 16 }}>{report.status}</Text>
+                            </View>
+                        </View>
+                        <Ionicons name='chevron-forward' size={size = 22} />
+                    </TouchableOpacity>
+                ))
+            )}
         </ScrollView>
     );
 }
@@ -19,5 +46,13 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         paddingHorizontal: 18,
         paddingTop: 18,
+    },
+    containerItens: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 5,
+    },
+    text: {
+        flexDirection: 'row',
     },
 });
