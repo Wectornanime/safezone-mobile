2ec1f6aba5573e04bfe53564a713c90f71099580
Wectornanime <81574689+Wectornanime@users.noreply.github.com>
2024-06-13 19:27:56 -0300

    added remove image button

--- src/screens/Report.js
+++ src/screens/Report.js
@@ -151,10 +151,23 @@ export default function Report({ navigation }) {
                         {imageUri && (
                             <Image source={{ uri: imageUri }} style={{
                                 width: '100%',
-                                height: '100%',
+                                height: 200,
                             }} />
                         )}
                     </TouchableOpacity>
+                    {imageUri && (
+                        <TouchableOpacity
+                            style={{
+                                width: 200,
+
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}
+                            onPress={() => setImageUri(null)}
+                        >
+                            <Text style={{ fontSize: 18, fontWeight: '600', color: 'red' }}>Remover imagem</Text>
+                        </TouchableOpacity>
+                    )}
 
                 </View>
 
